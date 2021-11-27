ac56d3fa2fefc18321598007692b7d21487e9a34
Mykhaylo Misyuk <jamesdarrel@MacBook-Air-Mykhaylo.local>
2021-11-27 16:09:24 +0200

    background.module.js updated

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,21 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
+  constructor(type, text) {
+    super(type, text)
+    this.backgroundColors = ['#0d43b8', '#007d17',
+                             '#0b6880', '#5b00c4',
+                             '#9d00c4', '#990000',
+                             '#00ab6c', '#1a1a1a']
+  }
 
+  getRandomColor() {
+    const index = Math.floor(Math.random() * this.backgroundColors.length)
+    return this.backgroundColors[index]
+  }
+
+  trigger() {
+    document.body.style.background = this.getRandomColor()
+    return document.body.style.transition = '1s'
+  }
 }
