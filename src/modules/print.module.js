bb4e9ee4eccfaa76653dff10c783475ec1f996ba
Mykhaylo Misyuk <jamesdarrel@MacBook-Air-Mykhaylo.local>
2021-11-28 18:36:13 +0200

    print module final cut

--- src/modules/print.module.js
+++ src/modules/print.module.js
@@ -8,7 +8,7 @@ export class PrintModule extends Module {
   }
 
   createInfoMessage() {
-    ourMessage = document.createElement('div')
+    const ourMessage = document.createElement('div')
     ourMessage.textContent = '{ Данная функция нужна для людей пожилого возраста }'
     ourMessage.style.fontSize = 'x-large'
     ourMessage.style.width = '100%'
@@ -24,7 +24,7 @@ export class PrintModule extends Module {
   }
 
   trigger() {
-    this.createInfoMessage()
+    this.displayInfoMessage()
     window.print()
   }
 }
