4497b86c7f4dd6c97fa8cc8043a155c3a7790bf2
Olga Shmakova <olg4983@gmail.com>
2021-11-28 12:01:42 +0200

    Implemented shape module-2

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -3,24 +3,30 @@ import * as Utils from '../utils';
 
 export class ShapeModule extends Module {
     #colors
+    #widthShape
+    #heightShape
+
     static TYPE = 'ShapeModule';
     static TEXT = 'Создать фигуру';
 
     constructor() {
     super(ShapeModule.TYPE, ShapeModule.TEXT)
-    this.#colors = [   '#E6D72A', '#5BC8AC', '#98DBC6', 
+    this.#colors = ['#E6D72A', '#5BC8AC', '#98DBC6', 
                     '#FEFE22', '#fi62ff', '#ffcce7', 
-                    '#fe3a9e', '#FBA0E3','#FF00FF', 
-                    '#DA70D6','#EEBEF1', '#CD00CD'];
+                    '#fe3a9e', '#FBA0E3', '#FF00FF', 
+                    '#DA70D6', '#EEBEF1', '#CD00CD'];
+    this.#widthShape = Utils.random(20, 60);
+    this.#heightShape = this.#widthShape * 1.5;               
     }
 
     getRendomColor = () => {
-        const index = Utils.random(0, this.#colors.length);
+        const index = Utils.random(0, this.#colors.length - 1);
         return this.#colors[index];
     }
 
     createNewShape = () => {
         const newShape = document.createElement('div');
+        newShape.id = 'shape';
         newShape.className = 'egg';
         newShape.display = 'block';
         newShape.style.webkitBorderRadius = '63px 63px 63px 63px / 108px 108px 72px 72px';
@@ -31,26 +37,29 @@ export class ShapeModule extends Module {
         newShape.style.marginTop = `${top}%`;
         newShape.style.marginLeft = `${left}%`;
     
-        const widthShape = Utils.random(20, 60);
-        const heightShape = widthShape * 1.5;
+        this.#widthShape = Utils.random(20, 60);
+        this.#heightShape = widthShape * 1.5;
         
-        newShape.style.width = `${widthShape}rem`; 
-        newShape.style.height = `${heightShape}rem`;
+        newShape.style.width = `${this.#widthShape}rem`; 
+        newShape.style.height = `${this.#heightShape}rem`;
     
         const color = this.getRendomColor();
         newShape.style.backgroundColor = color;
-    
-        return document.body.append(newShape);
+    }
+
+    displayNewShape = () => {
+
+       return document.body.append(this.createNewShape());
     }
 
     trigger() {
         document.body.addEventListener('click',() => {
-            const oldShape = document.querySelector('div');
+            const oldShape = document.querySelector('#shape');
             if(oldShape){
                 oldShape.remove();
             }
         
-            this.createNewShape();
+            this.displayNewShape();
         })
     }
 }
